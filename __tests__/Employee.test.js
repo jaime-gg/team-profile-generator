76601bfcbc52a3614a4ac104b8ce8fd8c9510508
jester <jaimebuddy98@gmail.com>
2022-01-27 15:42:12 -0600

    FIXED MY ERROR MESSAGE FINALLY

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,34 +1,42 @@
 const Employee = require('../lib/Employee.js') 
 
 describe('Employee',() => {
-    describe('generating the parent employee object',() => {
-        it('construct an employee object'), () => {
+    describe('generating the parent employee object', () => {
+        it('construct an employee object', () => {
             // contains name, id number, email
-            const employee = new Employee ('jay', 20,'jay@gmail.com')
+            const employee = new Employee('jaime', 20,'jaime@email.com')
 
             expect(employee.name).toEqual(expect.any(String));
             expect(employee.id).toEqual(expect.any(Number));   
             expect(employee.email).toEqual(expect.any(String));               
-        }
+        })
     })
     describe('getName',() => {
-        it('method for getting name'), () => {
+        it('method for getting name', () => {
+            const employee = new Employee('jaime', 20,'jaime@email.com')
 
-        }
+            expect(employee.getName).toEqual('jaime');
+        })
     })
     describe('getId',() => {
-        it('method for getting id'), () => {
+        it('method for getting id', () => {
+            const employee = new Employee('jaime', 20,'jaime@email.com')
 
-        }
+            expect(employee.getId).toEqual(20);
+        })
     })
     describe('getEmail',() => {
-        it('method for getting email'), () => {
+        it('method for getting email', () => {
+            const employee = new Employee('jaime', 20,'jaime@email.com')
 
-        }
+            expect(employee.getEmail).toEqual('jaime@email.com');
+        })
     })
     describe('getRole',() => {
-        it('returns employee'), () => {
+        it('returns employee', () => {
+            const employee = new Employee('jaime', 20,'jaime@email.com')
 
-        }
+            expect(employee.getRole).toEqual('Employee');
+        })
     })
 })
