cb6c1786954d176bc9391aadb85615f403b7a235
jester <jaimebuddy98@gmail.com>
2022-01-27 16:41:34 -0600

    manager passed all tests

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,14 +1,22 @@
+const { expect } = require('@jest/globals')
 const Manager = require('../lib/Manager.js') 
 
 describe('Manager',() => {
     describe('generating the Manager class',() => {
-        it('construct an Manager object'), () => {
-            // office number
-        }
+        it('construct an Manager object', () => {
+            // append office number to the end
+            const manager = new Manager('jaime', 20,'jaime@email.com', 2001);
+
+            expect(manager.officeNum).toEqual(2001);
+        })
     })
     describe('getRole',() => {
-        it('returns manager'), () => {
+        it('returns manager', () => {
+            const manager = new Manager('jaime', 20,'jaime@email.com', 2001);
 
-        }
+            // check the getRole() method from the manager class
+            // this should hopefully be overwritten to say manager rather then employee
+            expect(manager.getRole()).toEqual('Manager');
+        })
     })
 })
