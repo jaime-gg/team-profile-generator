7f526b8b2f631ccf6cc81fcf35f6482d22f2a058
jester <jaimebuddy98@gmail.com>
2022-01-27 18:10:14 -0600

    outlined the portions of the index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+// MODULES
+const fs = require('fs'); 
+const inquirer = require('inquirer');
+
+// TEAM CLASSES
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+
+// HTML PAGE GENERATOR 
+const generatePage = require('./src/generatePage');
+
+// TEAM
+const teamArr = []; 
+
+// QUESTIONS ------------------------------------------------------------------------------
+
+// MANAGER QUESTIONS
+
+
+// EMPLOYEE QUESTIONS
+
+// PAGE GENERATOR -------------------------------------------------------------------------
+
+// WRITE FILE FUNCTION
+
+// APPEND THE INPUTED DATA
