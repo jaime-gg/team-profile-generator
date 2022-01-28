0c1ed0320480a730451f52d6f41f4895cf92dffc
jester <jaimebuddy98@gmail.com>
2022-01-27 18:52:39 -0600

    added necssessary manager questions

--- index.js
+++ index.js
@@ -17,13 +17,78 @@ const teamArr = [];
 // QUESTIONS ------------------------------------------------------------------------------
 // MANAGER QUESTIONS
 const inputManager = () => {
+    return inquirer.prompt ([
+        // NAME
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Who is the manager for this team?', 
 
+            validate: name => {
+                if (name) {
+                    return true;
+                } else {
+                    console.log ("  Please enter a valid name!");
+                    return false; 
+                }
+            }
+        },
+        // ID
+        {
+            type: 'input',
+            name: 'id',
+            message: "What is the manager's employee ID?",
+            validate: id => {
+                if  (isNaN(id)) {
+                    console.log ("  Please enter a valid ID!")
+                    return false; 
+                } else {
+                    return true;
+                }
+            }
+        },
+        // EMAIL
+        {
+            type: 'input',
+            name: 'email',
+            message: "What is the email address of the manager?",
+            validate: email => {
+                if (email) {
+                    return true;
+                } else {
+                    console.log ('  Please enter a valid email!')
+                    return false; 
+                }
+            }
+        },
+        // OFFICE NUMBER
+        {
+            type: 'input',
+            name: 'office',
+            message: "Please enter the manager's office number",
+            validate: office => {
+                if  (isNaN(office)) {
+                    console.log ('  Please enter a valid office number!')
+                    return false; 
+                } else {
+                    return true;
+                }
+            }
+        }
+    ])
+    .then(managerData => {
+        const  { name, id, email, officeNumber } = managerData; 
+        const manager = new Manager (name, id, email, officeNumber);
+
+        teamArr.push(manager); 
+    })
 }
 
 // EMPLOYEE QUESTIONS
 const inputEmployee = () => {
     
 }
+
 
 // PAGE GENERATOR -------------------------------------------------------------------------
 // WRITE FILE FUNCTION
